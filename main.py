96181a436f4e628ee6f82a6897a038c5b1e715d0
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2025-10-04 15:55:46 +0300

    add basic structure

---
+++ main.py
@@ -0,0 +1,4 @@
+# from user_managment import user_login, user_register
+
+# user_register(input('Log: '), input('Pass: '))
+# user_login(input('Log: '), input('Pass: '))
---
+++ user_managment.py
@@ -0,0 +1,22 @@
+import hashlib
+
+users_db = {}
+
+hash_password = lambda password: hashlib.sha256(password.encode()).hexdigest()
+
+def user_register(username, password):
+    # Додаємо первинні перевірки паролю
+    if username in users_db:
+        print(f"Логін вже зайнятий")
+    else:
+        users_db[username] = hash_password(password)
+        
+def user_login(username, password):
+    # Додаємо первинні перевірки паролю
+    if username not in users_db:
+        print(f"Логіну не існує")
+    elif users_db[username] == hash_password(password):
+        print(f"Привіт, {username}!")
+        return True
+    else:
+        print("Невірний логін або пароль.")
