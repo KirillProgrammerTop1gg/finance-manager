a9a3fb7b3492775c7db4b4d6ebc72a1fb1e72738
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2025-10-05 18:15:13 +0300

    add currencies

--- finance.py
+++ finance.py
@@ -3,8 +3,9 @@ from typing import Optional, Union, Dict, List
 import time
 
 transactions: Dict[str, Dict[str, Union[int, float, str, List[str]]]] = {}
+currencies: Dict[str, float] = {}
 
-def add_transaction(balance_change: float, note: str, categories: Optional[List[str]] = None) -> str:
+def add_transaction(balance_change: float, note: str, currency: str = 'USD', categories: Optional[List[str]] = None) -> str:
     id = str(uuid.uuid4())
     if categories == None: categories = []
     categories.append('income' if balance_change > 0 else 'outcome')
@@ -26,11 +27,11 @@ def del_transaction(id: str) -> Optional[str]:
     
 def sum_txs(txs_list: List[Dict[str, Union[int, float, str, List[str]]]], category: Optional[List[str]] = None) -> float: return (txs_list[0]['balance_change'] if (category in txs_list[0]['categories'] or category == None) else 0) + sum_txs(txs_list[1:], category) if len(txs_list) else 0
     
-y = add_transaction(10, 'Super ', ['turtle'])
-x = add_transaction(-5, 'Test', ['b'])
-x = add_transaction(36, 'Test', ['turtle', 'b'])
+y = add_transaction(10, 'Super ', categories=['turtle'])
+x = add_transaction(-5, 'Test', categories=['b'])
+x = add_transaction(36, 'Test', categories=['turtle', 'b'])
 print(transactions)
 # edit_transaction(x, balance_change = -9, note = 'fu')
 # print(transactions)
 print(sum_txs(list(transactions.values())))
-print(sum_txs(list(transactions.values()),'g'))
+print(sum_txs(list(transactions.values()),'b'))
